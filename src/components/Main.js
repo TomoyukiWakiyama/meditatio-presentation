dc6e694254166e1058281cf79e1cb28222cfcf91
TomoyukiWakiyama <kreva2002@gmail.com>
2022-09-12 09:05:30 +0900

    20220912

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,27 @@
-import { Box, Heading, Image } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import React from "react";
-import Taikutsu from "../imgaes/taikutsu.svg";
-import Haveto from "../imgaes/haveto.svg";
-import Sight from "../imgaes/sight.svg";
 import SlideContent from "./page/SlideContent";
+import Taikutsu from "../imgaes/taikutsu.svg";
+import Macflog from "../imgaes/macflog.svg";
+import Sleep from "../imgaes/sleep.svg";
+import Spflog from "../imgaes/spflog.svg";
+import Kangaeru from "../imgaes/kanngaeru.svg";
+import Aseru from "../imgaes/aseru.svg";
+import Sight from "../imgaes/sight.svg";
+import Meditation from "../imgaes/meditation.svg";
+import Doudemoii from "../imgaes/doudemoii.svg";
+import Omoiukabu from "../imgaes/omoiukabu.svg";
+import Umaretekara from "../imgaes/umaretekara.svg";
+import Aruku from "../imgaes/aruku.svg";
+import Zeitaku from "../imgaes/zeitaku.svg";
+import Zeitaku2 from "../imgaes/zeitaku2.svg";
+import Focus from "../imgaes/focus.svg";
+import Tameshini from "../imgaes/tameshini.svg";
+import Yononaka from "../imgaes/yononaka.svg";
+import JibunTanin from "../imgaes/jibuntanin.svg";
+import House from "../imgaes/house.svg";
+import Oshimai from "../imgaes/oshimai.svg";
+import Muishiki from "../imgaes/muishiki.svg";
 
 const Main = () => {
   return (
@@ -21,55 +39,87 @@ const Main = () => {
         }}
       >
         <SlideContent title="瞑想は世界一退屈な行為" svg={Taikutsu} />
-        <SlideContent title="瞑想中は呼吸に意識を向ける" svg={Sight} />
+        <SlideContent
+          title="瞑想は呼吸に意識を向ける"
+          svg={Sight}
+          imgWidth="40%"
+        />
+        <SlideContent
+          title="試しに瞑想をやってみた"
+          svg={Tameshini}
+          imgWidth="30%"
+        />
 
-        <SlideContent title="試しに瞑想をやってみた" svg={Taikutsu} />
+        <SlideContent
+          title="そういえば、やらなきゃいけないことがある"
+          svg={Macflog}
+        />
+        <SlideContent
+          title="呼吸に意識をもどす"
+          svg={Meditation}
+          imgWidth="30%"
+        />
+        <SlideContent title="気になる動画がある" svg={Spflog} />
+        <SlideContent
+          title="呼吸に意識をもどす"
+          svg={Meditation}
+          imgWidth="20%"
+        />
+        <SlideContent title="横になって休みたい" svg={Sleep} />
+        <SlideContent
+          title="呼吸に意識をもどす"
+          svg={Meditation}
+          imgWidth="10%"
+        />
 
-        <SlideContent title="やらなきゃいけないことがある" svg={Haveto} />
-        <SlideContent title="呼吸に意識をもどす" svg={Taikutsu} />
-        <SlideContent title="横になって休みたい" svg={Taikutsu} />
-        <SlideContent title="呼吸に意識をもどす" svg={Taikutsu} />
-        <SlideContent title="気になる動画がある" svg={Taikutsu} />
-        <SlideContent title="呼吸に意識をもどす" svg={Taikutsu} />
-
-        <SlideContent title="瞑想はこんなことを繰り返す" svg={Taikutsu} />
+        <SlideContent title="瞑想はこんなことを繰り返す" svg={Aseru} />
         <SlideContent
           title="自分にとって一番どうでもいいことだから"
-          svg={Taikutsu}
+          svg={Doudemoii}
+          imgWidth="60%"
         />
-        <SlideContent title="いろいろなことが思い浮かぶ" svg={Taikutsu} />
+        <SlideContent
+          title="いろいろなことが思い浮かぶ"
+          svg={Omoiukabu}
+          imgWidth="60%"
+        />
 
-        <SlideContent title="でも、ふと思い返してみる" svg={Taikutsu} />
-        <SlideContent title="世の中に問題はたくさんある" svg={Taikutsu} />
-        <SlideContent title="自分のこと、他人のこと" svg={Taikutsu} />
-        <SlideContent title="人生に大切なことはいくつもある" svg={Taikutsu} />
-        <SlideContent title="意識的にも、無意識的にも" svg={Taikutsu} />
-
+        <SlideContent
+          title="でも、ふと思い返してみる"
+          svg={Kangaeru}
+          imgWidth="40%"
+        />
+        <SlideContent
+          title="世の中に問題はたくさんある"
+          svg={Yononaka}
+          imgWidth="40%"
+        />
+        <SlideContent title="自分のこと、他人のこと" svg={JibunTanin} />
+        <SlideContent
+          title="人生に大切なことはいくつもある"
+          svg={House}
+          imgWidth="40%"
+        />
+        <SlideContent title="意識的にも、無意識的にも" svg={Muishiki} />
         <SlideContent
           title="呼吸は生まれてから自然にできている"
-          svg={Taikutsu}
+          svg={Umaretekara}
+          imgWidth="30%"
         />
-        <SlideContent title="生まれてから続く、当たり前な行為" svg={Taikutsu} />
         <SlideContent
-          title="こんな自分の取るに足らないものに意識を向ける"
-          svg={Taikutsu}
+          title="知らぬ間に続く、当たり前な行為"
+          svg={Aruku}
+          imgWidth="40%"
         />
-        <SlideContent title="世界一退屈な時間" svg={Taikutsu} />
-        <SlideContent title="世界一贅沢な時間" svg={Taikutsu} />
+        <SlideContent
+          title="こんな自分の些細なことに意識を向ける"
+          svg={Focus}
+        />
+        <SlideContent title="世界一退屈な時間" svg={Zeitaku} />
+        <SlideContent title="世界一贅沢な時間" svg={Zeitaku2} />
         <SlideContent title="それが瞑想なのかもしれない" svg={Taikutsu} />
 
-        {/* <SlideContent title="瞑想で達成したい目標はない" svg={Taikutsu} />
-        <SlideContent
-          title="退屈だから、いつもやる気にはならない"
-          svg={Taikutsu}
-        />
-        <SlideContent title="でも、やった後は頭がじんわりする" svg={Taikutsu} />
-        <SlideContent title="その程度で十分な気がする" svg={Taikutsu} />
-        <SlideContent
-          title="だって、瞑想は世界一退屈な行為だから"
-          svg={Taikutsu}
-        /> */}
-        <SlideContent title="おしまい" svg={Taikutsu} />
+        <SlideContent title="おしまい" svg={Oshimai} />
       </Box>
     </>
   );
--- src/components/page/SlideContent.js
+++ src/components/page/SlideContent.js
@@ -1,29 +1,31 @@
-import { Box, Heading, Image } from "@chakra-ui/react";
+import { Box, Heading, Image, scaleFadeConfig } from "@chakra-ui/react";
 import React from "react";
 
-const SlideContent = ({ title, svg }) => {
+const SlideContent = ({ title, svg, imgWidth = "100%" }) => {
   return (
-    <Box w="80%" height="100vh" mx="auto" scrollSnapAlign="start">
+    <Box w="90%" height="100vh" mx="auto" scrollSnapAlign="start">
       <Heading
-        h="240px"
-        lineHeight="240px"
+        h="200px"
+        lineHeight="200px"
         textAlign="center"
+        fontSize="5xl"
         fontWeight="normal"
         letterSpacing={2}
       >
         {title}
       </Heading>
-      <Image
-        mx="auto"
-        h="calc(100vh - 300px)"
-        objectFit="cover"
-        src={svg}
-        alt=""
-        style={{
-          filter: "drop-shadow(0px -3px 16px rgba(0,0,0,0.3))",
-        }}
-        // dropShadow="30px 10px 4px #4444dd"
-      />
+      <Box w={imgWidth} mx="auto">
+        <Image
+          mx="auto"
+          h="calc(100vh - 220px)"
+          //   objectFit="cover"
+          src={svg}
+          alt=""
+          style={{
+            filter: "drop-shadow(0px -3px 16px rgba(0,0,0,0.3))",
+          }}
+        />
+      </Box>
     </Box>
   );
 };
